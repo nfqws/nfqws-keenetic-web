9d357c1f74e33875052b12421adcb34fa88e9f54
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2026-02-08 00:06:10 +0300

    dialog output fix

--- web/src/components/OutputLogDialog.tsx
+++ web/src/components/OutputLogDialog.tsx
@@ -4,11 +4,11 @@ import {
   Dialog,
   DialogActions,
   DialogContent,
-  DialogContentText,
   LinearProgress,
   Paper,
 } from '@mui/material';
 
+import { Editor } from '@/components/Editor';
 import { Trans } from '@/components/Trans';
 
 export const OutputLogDialog = ({
@@ -21,37 +21,24 @@ export const OutputLogDialog = ({
   onClose: VoidFunction;
 }) => {
   return (
-    <Dialog
-      open={open}
-      onClose={onClose}
-      maxWidth="sm"
-      fullWidth={true}
-      scroll="paper"
-      sx={{ maxHeight: '80svh' }}
-    >
+    <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth={true}>
       <DialogContent>
-        <DialogContentText component="div">
-          <Paper
-            variant="outlined"
-            sx={{
-              p: 2,
-              fontFamily: (theme) => theme.typography.mono.fontFamily,
-              fontSize: 13,
-              lineHeight: 1.5,
-              backgroundColor: 'background.default',
-              maxHeight: 300,
-              overflow: 'auto',
-            }}
-          >
-            <Box component="pre" sx={{ m: 0, whiteSpace: 'pre-wrap' }}>
-              {typeof content === 'string' ? (
-                content
-              ) : (
-                <LinearProgress color="inherit" />
-              )}
+        <Paper
+          variant="outlined"
+          sx={{
+            p: 0,
+            maxHeight: '70svh',
+            display: 'flex',
+          }}
+        >
+          {typeof content === 'string' ? (
+            <Editor type="log" value={content} readonly={true} />
+          ) : (
+            <Box flex={1} sx={{ p: 2, backgroundColor: 'background.default' }}>
+              <LinearProgress color="inherit" />
             </Box>
-          </Paper>
-        </DialogContentText>
+          )}
+        </Paper>
       </DialogContent>
       <DialogActions
         sx={{
